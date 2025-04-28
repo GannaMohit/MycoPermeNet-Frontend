c33d75d157d8e68ee2a251d94640b8c7e55e043a
Mohit Ganna <mohitganna@gmail.com>
2025-04-28 03:33:04 -0400

    Integrated PrimeReact

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,10 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
-    </html>
+    <PrimeReactProvider>
+      <html lang="en">
+        <body>
+          {children}
+        </body>
+      </html>
+    </PrimeReactProvider>
   );
 }
